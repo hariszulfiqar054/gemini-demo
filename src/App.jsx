b8fcc551ad5dd9b6afe99d44523795020a2b8bf9
Haris <m.haris@calo.app>
2024-03-27 01:36:15 +0500

    added ingredients and states

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Ingredients } from './ingredients';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedIngredients, setSelectedIngredients] = useState([]);
+
+  const handleIngredientSelector = (ingredient) => {
+    const exists = selectedIngredients.includes(ingredient);
+    if (exists) {
+      setSelectedIngredients(
+        ...[selectedIngredients.filter((value) => value !== ingredient)]
+      );
+    } else {
+      setSelectedIngredients([...selectedIngredients, ingredient]);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div>
+      {Ingredients.map((value) => (
+        <button
+          onClick={() => handleIngredientSelector(value)}
+          key={value}
+          style={{
+            backgroundColor: selectedIngredients.includes(value)
+              ? 'yellow'
+              : 'salmon',
+            margin: '12px',
+            color: selectedIngredients.includes(value) ? 'black' : 'white',
+          }}
+        >
+          {value}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
